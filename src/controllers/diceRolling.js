e7bb701f9c6d16ebe3e36c052eb1a8844401eeee
burningpigeon <pidgepurr@gmail.com>
2026-02-12 19:48:00 -0400

    added huntingRoll function to diceRolling.js

--- src/controllers/diceRolling.js
+++ src/controllers/diceRolling.js
@@ -136,3 +136,72 @@ function getRandomPrey(categoryIn){
 
 // const randomAirPrey = getRandomPrey('air');
 // console.log(randomAirPrey)
+
+function huntingRoll(levelIn, categoryIn){
+    const failCheck = huntingFailCheck(levelIn)
+    const modifier = Math.floor(parseInt(getModifier(levelIn)))
+    if (failCheck === true){
+        return{
+            success: true,
+            message: "Unfortunately, you don't find any prey."
+        }
+    }
+    else{
+        const roll = Math.floor(parseInt(roll20()));
+        const preyRoll = getRandomPrey(categoryIn);
+        if (roll === 1){
+            return{
+                success: true,
+                message: " Natural [1] - Ouch! You lose the prey, and somehow hurt yourself in the process. This results in an injury level of your choosing! If you are currently injured, ignore this and instead treat it as a regular failed roll with no negative outcomes."
+            }
+        }
+        else if (roll === 20){
+            return{
+                success: true,
+                message: `🍖 You caught sight of a ${preyRoll.emoji} ${preyRoll.prey_name}! 🌟 Natural [20] - You catch the prey excellently - and it's absolutely massive! This prey counts as two pieces. Please submit it to your Clan's Freshkill pile before Monday!`
+            }
+        }
+        else{
+            const finalRoll = roll + modifier;
+            if (finalRoll <= 1){
+                return{
+                    success: true,
+                    message: `🐾 [${finalRoll}] Terrible Hunt: You stumble over your own paws. Luckily, you are unhurt, but the prey is long gone.`
+                    }
+                }
+            else if (finalRoll <=3){
+                return{
+                    success: true,
+                    message: `🐾 [${finalRoll}] Lousy Hunt: You miss the kill sloppily. Might get wet if fishing, or covered in dirt if on the ground.`
+                    }
+                }
+            else if (finalRoll <=6){
+                return{
+                    success: true,
+                    message: `🐾 [${finalRoll}] Almost Got It: You almost caught it! You miss by a whisker length!`
+                    }
+                }
+            else if (finalRoll <=14){
+                return{
+                    success: true,
+                    message: `🍖 You caught sight of a ${preyRoll.emoji} ${preyRoll.prey_name}! 🐾 [${finalRoll}] Mediocre Hunt: You catch your prey, but not very well. It’s a bit mangled. Please submit it to your Clan's Freshkill pile before Monday!`
+                    }
+                }
+            else if (finalRoll <=19){
+                return{
+                    success: true,
+                    message: `🍖 You caught sight of a ${preyRoll.emoji} ${preyRoll.prey_name}! 🐾 [${finalRoll}] Good Hunt: Decently executed hunt! Still some minor room for improvements. Please submit it to your Clan's Freshkill pile before Monday!`
+                    }
+                }
+            else if (finalRoll <= 20){
+                return{
+                    success: true,
+                    message: `🍖 You caught sight of a ${preyRoll.emoji} ${preyRoll.prey_name}! 🐾 [${finalRoll}] Excellent Hunt: Perfectly executed hunt, couldn't have gone better. Please submit it to your Clan's Freshkill pile before Monday!`
+                    }
+                }
+            }
+        }
+    }
+
+output = huntingRoll("Mastered","air")
+console.log(output)
